64509f88b24b6a2983457fa95205f6c8fa872e04
leo0807 <zhangjunxu3@gmail.com>
2020-11-19 22:36:57 +0800

    readonly part

--- design-pattern/src/decro-es7.js
+++ design-pattern/src/decro-es7.js
@@ -7,3 +7,49 @@ function testDec(isDec){
 class Demo{}
 
 alert(Demo.isDec);
+
+function mixins(...list){
+    return function(target){
+        Object.assign(target.prototype, ...list);
+    }
+}
+
+const Foo = {
+    foo(){
+        alert('foo');
+    }
+}
+@mixins(Foo)
+class MyClass{}
+
+let obj = new MyClass();
+obj.foo();
+// 装饰方法 @readonly 只读方法
+function readonly(target, name, description){
+    description.writable = false;
+    return description;
+}
+
+function log(target, name, description){
+    var oldValue = descriptor.value;
+
+    descriptor.value = function(){
+        console.log(`Calling ${name} with`, arguments);
+        return oldValue.apply(this, arguments);
+    }
+    return descriptor;
+}
+
+
+class Person{
+    constructor(){
+        this.first = 'A';
+        this.last = 'B';
+    }
+    @readonly
+    name(){
+        return `${this.first} ${this.last}`;
+    }
+}
+const p = new Person();
+console.log(p.name());
