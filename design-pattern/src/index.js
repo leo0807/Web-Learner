12130dfe708147ac863e015261ca0522a3eac9f5
leo0807 <zhangjunxu3@gmail.com>
2020-11-19 14:26:23 +0800

    OOP

--- design-pattern/src/index.js
+++ design-pattern/src/index.js
@@ -1,4 +1,30 @@
-const changeBgc = () =>{
-    document.body.style.backgroundColor = 'red';
-}
-changeBgc();
+// const changeBgc = () =>{
+//     document.body.style.backgroundColor = 'red';
+// }
+// changeBgc();
+// // OOP
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     eat(){
+//         alert(`${this.name} eat something`);
+//     }
+//     speak(){
+//         alert(`${this.name} \`s age is ${this.age}`);   
+//     }
+// }
+// // Inh
+// class Student extends People{
+//     constructor(name, age, number){
+//         super(name, age);
+//         this.number = number;
+//     }
+//     study(){
+//         alert(`Student Number ${this.number} is learning`);
+//     }
+// }
+
+
