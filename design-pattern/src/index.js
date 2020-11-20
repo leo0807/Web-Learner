219a395e2ba9be3f03f79b956d23190a39258c65
leo0807 <zhangjunxu3@gmail.com>
2020-11-20 16:58:56 +0800

    initialization

--- design-pattern/src/index.js
+++ design-pattern/src/index.js
@@ -28,34 +28,38 @@
 // }
 
 
-function loadImg(src){
-    let promise = new Promise(function(resolve, reject){
-        const img = document.createElement('img');
-        img.onload = function(){
-            resolve(img);
-        };
-        img.onerror = function(){
-            reject('Loading Failed');
-        };
-    });
-    return promise;
-}
-const src = 'https://uploadfiles.nowcoder.com/images/20191018/63_1571399271580_F19C9085129709EE14D013BE869DF69B';
-let result = loadImg(src);
+// function loadImg(src){
+//     let promise = new Promise(function(resolve, reject){
+//         const img = document.createElement('img');
+//         img.onload = function(){
+//             resolve(img);
+//         };
+//         img.onerror = function(){
+//             reject('Loading Failed');
+//         };
+//     });
+//     return promise;
+// }
+// const src = 'https://uploadfiles.nowcoder.com/images/20191018/63_1571399271580_F19C9085129709EE14D013BE869DF69B';
+// let result = loadImg(src);
 
-result.then(function(img){
-    alert(`width: ${img.width}`);
-    return img;
-}).then(function(img){
-    alert(`height: ${img.height}`);
-    return img;
-}).then(
-    function()
-    {
-        alert(img.src);
-        return img;
-    }
-).catch(function(err){
-    alert(err);
-})
+// result.then(function(img){
+//     alert(`width: ${img.width}`);
+//     return img;
+// }).then(function(img){
+//     alert(`height: ${img.height}`);
+//     return img;
+// }).then(
+//     function()
+//     {
+//         alert(img.src);
+//         return img;
+//     }
+// ).catch(function(err){
+//     alert(err);
+// })
 
+import App from './demo/App.js';
+
+const app = new App('app');
+app.init();
