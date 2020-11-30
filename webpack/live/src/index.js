99e7d07b9eb6a5478f97f764394a7814146b291e
leo0807 <zhangjunxu3@gmail.com>
2020-11-30 22:21:00 +0800

    update webpack

--- webpack/live/src/index.js
+++ webpack/live/src/index.js
@@ -1,5 +1,5 @@
 import data from './data.json';
-// 引入样式资源
+
 import './style.css';
 function fn1() {
     console.log('fn1');
@@ -7,7 +7,3 @@ function fn1() {
 fn1();
 console.log(data);
 
-// 开发环境下的打包
-// webpack ./src/index.js -o ./dist/bundle.js --mode=development
-// 生产环境
-// webpack ./src/index.js -o ./dist/bundle.js --mode=production
