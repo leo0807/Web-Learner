1ff112562b767941c825e68a58a2d32e138a7ac9
leo0807 <zhangjunxu3@gmail.com>
2020-12-05 23:45:56 +0800

    Sub Router

---
+++ Vue/v-router/src/components/HomeNews.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li>News</li>
+      <li>News</li>
+      <li>News</li>
+      <li>News</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeNews"
+}
+</script>
+
+<style scoped>
+
+</style>
+
