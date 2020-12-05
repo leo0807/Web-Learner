ca8ef07e0f28de2ff8221b8eb7472d55d9c0c9bb
leo0807 <zhangjunxu3@gmail.com>
2020-12-06 00:06:42 +0800

    User Component

---
+++ Vue/v-router/src/components/User.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+  <h2>User Page</h2>
+  <h2>{{userId}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  computed:{
+    userId(){
+      return this.$route.params.userId
+      // $route 取得处于活跃路由的对象
+    },
+    beforeRouteEnter (to, from, next) {
+      console.log("组件内部导航守卫");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
