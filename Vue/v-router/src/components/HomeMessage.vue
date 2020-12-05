09d796f27ec62a30e3fa8139ce9b09eedf044a63
leo0807 <zhangjunxu3@gmail.com>
2020-12-05 23:45:23 +0800

    Sub Router

---
+++ Vue/v-router/src/components/HomeMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <ul>
+    <li>Message</li>
+    <li>Message</li>
+    <li>Message</li>
+    <li>Message</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeMessage"
+}
+</script>
+
+<style scoped>
+
+</style>
+
