02190ca5f8b7cababd6a088fa4b53529e5986607
leo0807 <zhangjunxu3@gmail.com>
2020-12-06 00:10:18 +0800

    Home Component

--- Vue/v-router/src/components/Home.vue
+++ Vue/v-router/src/components/Home.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
   <h2>Home Page</h2>
+  <router-link to="/home/news">News</router-link>
+  <router-link to="/home/message">Message</router-link>
+  <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      path: '/home/news'
+    }
+  },
+  // 只有使用keep-alive的包裹的时候 以下两个函数才有用
+  // activated() {
+  //   this.$router.push(this.path)
+  // },
+  //  deactivated() {
+  //   this.path = this.$route.path;
+  // },
+  beforeRouteLeave (to, from, next) {
+    this.path = this.$route.path;
+}
 }
 </script>
 
