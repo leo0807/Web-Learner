7a272ccf750ab5bf17783fa2a7d2ab26711bc005
leo0807 <zhangjunxu3@gmail.com>
2020-12-06 00:08:35 +0800

    Profile Component

---
+++ Vue/v-router/src/components/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+  <h2>Profile Page</h2>
+  <h2>{{$route.query.name}}</h2>
+  <h2>{{$route.query.age}}</h2>
+  <h2>{{$route.query.height}}</h2>
+  <h2 v-for="item in $route.query">{{item}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile"
+}
+</script>
+
+<style scoped>
+
+</style>
