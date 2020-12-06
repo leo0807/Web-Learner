55862efc3f64fbf7572ae15f67bbd861c0562dd9
leo0807 <zhangjunxu3@gmail.com>
2020-12-06 15:36:06 +0800

    tabbar usage

---
+++ Vue/tabbar/src/components/tabbar/basic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="tab-bar">
+    <div class="tab-bar-item">首页</div>
+    <div class="tab-bar-item">分类</div>
+    <div class="tab-bar-item">购物车</div>
+    <div class="tab-bar-item">我的</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
