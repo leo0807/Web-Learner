3e88beda5da2752e659fba9d0c564c04a6036679
leo0807 <zhangjunxu3@gmail.com>
2020-12-07 17:19:28 +0800

    insertion sort

--- Interview/Sort-Algorithms/array.js
+++ Interview/Sort-Algorithms/array.js
@@ -5,11 +5,11 @@ function ArrayList() {
         this.array.push(item);
     }
     ArrayList.prototype.toString = function () {
-        this.array.join('-');
+        return this.array.join('-');
     }
 
     ArrayList.prototype.swap = function (m, n) {
-        var temp = m;
+        var temp = this.array[m];
         this.array[m] = this.array[n];
         this.array[n] = temp;
     }
@@ -21,7 +21,7 @@ function ArrayList() {
         // 两两比较 每次最大的值放在最右边
         // 第一次 j = length - 1， 比较到倒数第一个位置
         // 第一次 j = length - 2， 比较到倒数第二个位置
-        for (var j = length - 1; j >= 0; j++){
+        for (var j = length - 1; j >= 0; j--){
             for (var i = 0; i < length - 1; i++){
                 if (this.array[i] > this.array[i + 1]) {
                     this.swap(i, i + 1);  
@@ -48,10 +48,37 @@ function ArrayList() {
                     min = i
                 }
             }
-            this.swap(j, min);
+            this.swap(min, j);
         }
-
     }
+    // 比较次数虽然和冒泡排序一样 但是N项只需要交换N-1次
 
-    // 插入排序
+    // 插入排序是简单排序中效率最好的
+    ArrayList.prototype.insertionSort = function () {
+        var length = this.array.length;
+        // 从第一个位置获取数据，向前面局部有序进行插入
+        for (var j = 1; j < length; j++){
+
+            var temp = this.array[j];
+            // 内层循环，获取i位置的元素，和前面的元素依次比较
+            while (this.array[j - 1] > temp && j > 0) {
+                this.array[j] = this.array[j - 1];
+                j--;
+            }
+            this.array[j] = temp;
+        }
+    }
 }
+var list = new ArrayList();
+
+list.insert(32);
+list.insert(4);
+list.insert(231);
+list.insert(42);
+list.insert(2);
+list.insert(6);
+console.log(list);
+// list.bubbleSort();
+// list.selectionSort();
+list.insertionSort();
+console.log(list.toString());
