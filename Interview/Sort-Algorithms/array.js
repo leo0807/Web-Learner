75d9e6b202d0316cd4bba87048211c296408b0bb
leo0807 <zhangjunxu3@gmail.com>
2020-12-07 16:22:41 +0800

    selection sort

--- Interview/Sort-Algorithms/array.js
+++ Interview/Sort-Algorithms/array.js
@@ -18,6 +18,7 @@ function ArrayList() {
     // 冒泡排序 Bubble Sort
     ArrayList.prototype.bubbleSort = function () {
         var length = this.array.length;
+        // 两两比较 每次最大的值放在最右边
         // 第一次 j = length - 1， 比较到倒数第一个位置
         // 第一次 j = length - 2， 比较到倒数第二个位置
         for (var j = length - 1; j >= 0; j++){
@@ -27,8 +28,30 @@ function ArrayList() {
                 }
             }
         }
+        // 效率 N^2/2 - N/2 => O(n^2)
     }
     // 选择排序
+    // 对冒泡排序进行优化 交换次数由O(n^2) 变成 O(n)
+    // 但是比较的次数依然是O(n^2)
+    // 方法 选择一个最小值和第一个索引位进行交换，此时第一个索引位置为最小值
+    // 剩下的元素一次类推
 
-    // 
+    ArrayList.prototype.selectionSort = function () {
+        var length = this.array.length;
+
+        for (var j = 0; j < length - 1; j++){
+
+            var min = j;
+            // 从后一位开始， 从0位置开始取数据
+            for (var i = min + 1; i < length; i++){
+                if (this.array[min] > this.array[i]) {
+                    min = i
+                }
+            }
+            this.swap(j, min);
+        }
+
+    }
+
+    // 插入排序
 }
