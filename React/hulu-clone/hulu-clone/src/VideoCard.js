6251465a4b0d6658921f310213d26db51f6fd12f
leo0807 <zhangjunxu3@gmail.com>
2020-12-09 07:52:05 +0800

    updates

---
+++ React/hulu-clone/hulu-clone/src/VideoCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoCard.css'
+function VideoCard(props) {
+    return (
+        <div className="videoCard">
+            <img src="" alt="" />
+            <P></P>
+        </div>
+    )
+}
+
+export default VideoCard
