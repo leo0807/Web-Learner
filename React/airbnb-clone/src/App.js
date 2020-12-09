a791936b68443aa1256809253af43038b84598ed
leo0807 <zhangjunxu3@gmail.com>
2020-12-09 20:15:26 +0800

    update clone

--- React/airbnb-clone/src/App.js
+++ React/airbnb-clone/src/App.js
@@ -2,12 +2,22 @@ import './App.css';
 import Home from './Home';
 import Header from './Header'
 import Footer from './Footer'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import SearchPage from './SearchPage'
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+      </Switch>
       <Footer />
+      <Router />
     </div>
   );
 }
