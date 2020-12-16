b5f8de5a8ec53d78485a10418e04a05f49c563fe
leo0807 <zhangjunxu3@gmail.com>
2020-12-16 21:22:20 +0800

    instanceof

---
+++ Interview/JavaScript/手写JS/instanceof.js
@@ -0,0 +1,12 @@
+function myInstanceof(left, right){
+    if(typeof left !== 'object' || left === null) return false;
+    // 获得对象的原型对象
+    proto = Object.getPrototypeOf(left);
+    while(true){
+        // 查到头未找到
+        if(proto === null) reutn false;
+        if(proto === right.prototype) return true;
+        // 继续寻找
+        proto = Object.getPrototypeOf(left);
+    }
+}
