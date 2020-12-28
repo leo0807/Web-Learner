3e0c4499c34060535f7b323e0e17dab0a5e7c8d2
leo0807 <zhangjunxu3@gmail.com>
2020-12-28 23:05:02 +0800

    .

--- Interview/Leetcode/剑指offer/螺旋矩阵.js
+++ Interview/Leetcode/剑指offer/螺旋矩阵.js
@@ -15,15 +15,17 @@ function spiralOrder(matrix) {
             top++;
             direction = "down";
         }
-        else if (direction = "down") {
+        else if (direction === "down") {
             for (let i = top; i <= bottom; i++) {
                 res.push(matrix[i][right]);
             }
             right--;
             direction = "left";
         }
-        else if (direction = "left") {
+        else if (direction === "left") {
+            console.log(1);
             for (let i = right; i >= left; i--) {
+                console.log(matrix[bottom][i]);
                 res.push(matrix[bottom][i]);
             }
             bottom--;
@@ -40,4 +42,5 @@ function spiralOrder(matrix) {
     return res;
 }
 const m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// const m = [[1, 2, 3, 3], [4, 5, 6, 3]]
 console.log(spiralOrder(m));
