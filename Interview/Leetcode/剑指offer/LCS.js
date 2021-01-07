c5a09943efce154b1b7965263c0d3c0780260b9c
leo0807 <zhangjunxu3@gmail.com>
2021-01-07 14:47:08 +0800

    .

--- Interview/Leetcode/剑指offer/LCS.js
+++ Interview/Leetcode/剑指offer/LCS.js
@@ -3,7 +3,7 @@ function LCS(str1, str2) {
     const length1 = str1.length;
     const length2 = str2.length;
     let dp = new Array(length1 + 1);
-    for (let i = 0; i < length1; i++) {
+    for (let i = 0; i <= length1; i++) {
         dp[i] = new Array(length2 + 1).fill(0);
     }
     let index = 0,
@@ -19,9 +19,12 @@ function LCS(str1, str2) {
             }
         }
     }
+    console.log(dp[length1][length2]);
     return str1.substring(index - max, index)
 }
-let str1 = "1AB2345CD",
-    str2 = "12345EF";
-
+// let str1 = "1AB2345CD",
+//     str2 = "12345EF";
+const str1 = "1A2C3D4B56",
+    str2 = "B1D23CA45B6A";
+// "1A2C3D4B56", "B1D23CA45B6A"
 console.log(LCS(str1, str2));
