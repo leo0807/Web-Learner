c0a4859f40eb144174d12fd09efe77869f8621a4
leo0807 <zhangjunxu3@gmail.com>
2021-01-29 21:30:28 +0800

    .

--- Backend/nodeJS/os/app.js
+++ Backend/nodeJS/os/app.js
@@ -1,6 +1,19 @@
-const os = require('os');
+// const os = require('os');
 // Check CPU Info
 // console.log(os.cpus());
 // check RAM
-console.log(os.totalmem());
-console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.freemem());
+
+var http = require('http');
+var port = 3000;
+// 创建io对象
+var io = require('socket.io')(server);
+io.on("connection", function () {
+    console.log('Client end point has connected');
+})
+var server = http.createServer(function (req, res) {
+    res.end("Hello");
+})
+
+server.listen(port, "127.0.0.1");
