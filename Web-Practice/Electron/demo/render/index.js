162faebb39ac33a5bc67d206c1b500582a12c17a
leo0807 <zhangjunxu3@gmail.com>
2021-03-25 11:58:40 +0800

    delete uesless files

--- Web-Practice/Electron/demo/render/index.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-window.onload = function(){
-    var btn = this.document.querySelector('#btn');
-    var myDiv = this.document.querySelector('#myDiv');
-    fs.readFile('nameList.txt', (err, data) =>{
-        myDiv.innerHTML = data;
-    })
-}
