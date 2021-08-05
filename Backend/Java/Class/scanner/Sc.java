d88e331515d5447d42427921288d5c0a2193912c
leo0807 <zhangjunxu3@gmail.com>
2021-08-05 23:33:35 +0800

    fix errors

--- Backend/Java/Class/scanner/Sc.java
+++ Backend/Java/Class/scanner/Sc.java
@@ -2,14 +2,14 @@ package Backend.Java.Class.scanner;
 
 import java.util.Scanner;
 
-public class scanner {
+public class Sc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Use nextLine method to receive message");
-        if(scanner.hasNextLine()){
+        if (scanner.hasNextLine()) {
             String str = scanner.nextLine();
             System.out.println("The output content is :" + str);
         }
         scanner.close();
-    }    
+    }
 }
