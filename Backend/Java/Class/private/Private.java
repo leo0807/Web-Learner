e91ac43765743432b97b2c0ea6cd2b1f0af99a22
leo0807 <zhangjunxu3@gmail.com>
2021-08-09 23:14:10 +0800

    private

---
+++ Backend/Java/Class/private/Private.java
@@ -0,0 +1,22 @@
+public class Private {
+    private String name;
+    private int id;
+    private char sex;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+}
