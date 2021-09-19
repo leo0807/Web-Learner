eaec91116fd858d5486d014d0d87ca3c11adab6e
leo0807 <zhangjunxu3@gmail.com>
2021-09-19 11:59:54 +0800

    update

---
+++ Frontend/ThreeJS/05-transform-objects/src/script.js
@@ -0,0 +1,88 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+mesh.position.x = 0.7
+mesh.position.y = 0.6
+mesh.position.z = 0.5
+// mesh.position.set(0.7, 0.6, 0.5)
+// Scale
+mesh.scale.x = 2
+mesh.scale.y = 0.5
+mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+// Rotation
+mesh.rotation.reorder('YXZ') //旋转顺序，默认XYZ
+mesh.rotation.y = 0.5
+mesh.rotation.x = 0.5
+mesh.rotation.z = 0.5
+// mesh.rotation.y = Math.PI 转半圈
+
+scene.add(mesh)
+
+// Objects
+const group = new THREE.Group()
+group.position.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube2.position.x = -2
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube3.position.x = 2
+group.add(cube3)
+
+// Axes Helper 添加坐标轴辅助 ,构造函数为坐标轴长度
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+// 更改Camera看的方向，默认为scene中心
+camera.lookAt(new THREE.Vector3(3, 0, 0))
+camera.lookAt(mesh.position)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
