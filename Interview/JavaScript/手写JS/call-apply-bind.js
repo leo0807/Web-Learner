31245378eaac8009cdce26cf5c6be70239e3feb6
leo0807 <zhangjunxu3@gmail.com>
2021-10-13 16:11:37 +0800

    update

--- Interview/JavaScript/手写JS/call-apply-bind.js
+++ Interview/JavaScript/手写JS/call-apply-bind.js
@@ -74,6 +74,30 @@ Function.prototype.myBind = function (objThis, ...args) {
 // bind() 方法会创建一个新函数。当这个新函数被调用时，bind() 的第一个参数将作为它运行时的 this，
 // 之后的一序列参数将会在传递的实参前传入作为它的参数。(来自于 MDN )
 
+/**
+ * bind方法是ES5 新增的一个方法，传参和call方法一致。与call、apply方法的区别是，call和apply方法会对目标函数进行自动执行，会返回一个新的函数。
+ * call和apply无法在事件绑定函数中使用。
+ */
+// E.g.
+
+const obj = { name: 'pixel' };
+document.addEventListener('click', onClick.bind(obj, 22, 66), false); // pixel 22 66
+function onClick(p1, p2) {
+    console.log(this.name, p1, p2);
+}
+
+// E.g.2
+
+const button = document.getElementById('button');
+const text = document.getElementById('text');
+button.onclick = function () {
+    console.log(this.id);
+}.bind(text); //改变this指向
+
+/**
+ * 1 bind操作只有第一次绑定有效果，之后再次进行绑定，不会有效果；操作的只是它的代理。
+ * 2 new操作的绑定操作优先度要高于bind，事实上，也会高于apply和call
+ */
 function myBind() {
     const args = Array.from(arguments);
     const _this = args.shift();
@@ -83,18 +107,18 @@ function myBind() {
     }
 }
 
-function myBind2(context){
+function myBind2(context) {
     const self = this;
-    return function(){
+    return function () {
         return self.apply(context);
     }
 }
 
-function myBind3(context){
+function myBind3(context) {
     let self = this;
     let args = Array.prototype.slice.call(arguments, 1);
 
-    return function(){
+    return function () {
         let bindArgs = Array.prototype.slice.call(arguments);
         return self.apply(context, args.concat(bindArgs));
     }
@@ -102,18 +126,18 @@ function myBind3(context){
 
 
 
-Function.prototype.bindFunc = function(context){
-    if(typeof this !== 'function'){
+Function.prototype.bindFunc = function (context) {
+    if (typeof this !== 'function') {
         throw new Error('xxx');
     }
     let self = this;
     let args = Array.prototype.slice.call(arguments, 1);
 
-    let fNOP = function(){};
+    let fNOP = function () { };
 
-    let fBound = function(){
+    let fBound = function () {
         let bindArgs = Array.prototype.slice.call(arguments);
-        return self.apply(this instanceof fNOP? this: context, args.concat(bindArgs));
+        return self.apply(this instanceof fNOP ? this : context, args.concat(bindArgs));
     }
     fNOP.prototype = this.prototype;
     fBound.prototype = new fNOP();
