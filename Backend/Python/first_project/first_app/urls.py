090e13d0fc22240828953dea6b3ec8f6d3d44f9f
leo0807 <zhangjunxu3@gmail.com>
2022-03-17 08:35:51 +0800

    Cloud Computing

--- Backend/Python/first_project/first_app/urls.py
+++ Backend/Python/first_project/first_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('', views.index,name="index"),
-    path('home/',views.home,name="home"),
-    path('educative/',views.educative,name="educative"),
+    path('', views.index, name="index"),
+    path('home/', views.home, name="home"),
+    path('educative/', views.educative, name="educative"),
+    path('forms/', views.forms, name="form"),
 ]
--- Backend/Python/first_project/first_app/views.py
+++ Backend/Python/first_project/first_app/views.py
@@ -1,31 +1,51 @@
+from .forms import TestForm
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import SearchForm
+from django.contrib.auth.models import User
+
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+
 # Create your views here.
 
 
 def index(request):
-    my_dict = {'insert_me': "Hello I am from views.py!"}
-    app_name = 'zing_it'
     return render(request, 'first_app/index.html')
 
 
-def even_or_odd(request, num):
-    if(num % 2 == 0):
-        output = "%s is an even number." % num
-    else:
-        output = "%s is an odd number." % num
-    return HttpResponse(output)
-
-
 def home(request):
-    return HttpResponse("Welcome to home page!")
+    try:
+        user = User.objects.create_user(
+            username="Ulrich", email="ulrich@gmail.com", password="mac12")
+        user.save()
+    except IntegrityError as e:
+        print(e)
+    return HttpResponse("Welcome to Home page!")
 
 
 def educative(request):
+    user = User.objects.get(username='Ulrich')
+    user.email = "helsinki@gmail.com"
+    user.save()
     return HttpResponse("Welcome to Educative page!")
 
 
+# def forms(request):
+#     form = SearchForm()
+#     return render(request, 'first_app/forms.html', {'form': form})
+
+
 def forms(request):
-    form = SearchForm()
-    return render(request, 'first_app/forms.html', {'form': form})
+
+    inital_dict = {
+        "text": "Some initial data",
+        "integer": 123,
+    }
+    # form = TestForm(request.POST or None)
+    form = TestForm(request.POST or None, initial=inital_dict)
+    data = "None"
+    text = "None"
+    if form.is_valid():
+        data = form.cleaned_data
+        text = form.cleaned_data.get("text")
+    return render(request, 'first_app/forms.html', {'form': form, 'data': data, 'text': text})
